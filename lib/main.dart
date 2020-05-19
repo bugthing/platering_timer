c09390c6666517c1907ece2d20b9ae0f9c57d32b
Benjamin Martin <ben@madeofpaper.co.uk>
2020-05-19 22:52:31 +0100

    Take a step back:

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,34 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-//import 'models/timer_model.dart';
 
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
 
-//void main() {
-//  runApp(new MaterialApp(
-//    debugShowCheckedModeBanner: false,
-//    home: new MyApp(),
-//  ));
-//}
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  MyAppState createState() => new MyAppState();
-}
-
-class MyAppState extends State<MyApp> {
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Plastering Timer'),
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('Plastering Timer'),
+          centerTitle: true,
+          backgroundColor: Colors.blue[600]
       ),
-      body: new Container(
-        color: Colors.blueAccent,
-        padding: new EdgeInsets.all(32.0),
-        child: new MyAppLayout(),
+      body: Center(
+        child: Text(
+          'Lets time some plastering',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+          ),
+        ),
       ),
-    );
-  }
-}
-
-class MyAppLayout extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        new ProgressIndicatorDemo(),
-        Text('----------'),
-        //TimerModel().items,
-        MaterialButton(
-          child: Text('Go'),
-          shape: CircleBorder(),
-          padding: EdgeInsets.all(30.0),
-          color: Colors.greenAccent,
-          onPressed: () {
-            //Do something
-          },
-        )
-      ],
-    );
-  }
-}
-
-class StageRow extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Icon(Icons.home),
-      ],
-    );
-  }
-}
-
-
-class ProgressIndicatorDemo extends StatefulWidget {
-
-  @override
-  _ProgressIndicatorDemoState createState() =>
-      new _ProgressIndicatorDemoState();
-}
-
-class _ProgressIndicatorDemoState extends State<ProgressIndicatorDemo>
-    with SingleTickerProviderStateMixin {
-  AnimationController controller;
-  Animation<double> animation;
-
-  @override
-  void initState() {
-    super.initState();
-    controller = AnimationController(
-        duration: const Duration(milliseconds: 2000), vsync: this);
-    animation = Tween(begin: 0.0, end: 1.0).animate(controller)
-      ..addListener(() {
-        setState(() {
-          // the state that has changed here is the animation object’s value
-        });
-      });
-    controller.repeat();
-  }
-
-  @override
-  void dispose() {
-    controller.stop();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Center(
-        child: new Container(
-          child:  LinearProgressIndicator( value:  animation.value,),
-
-        )
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.green[600],
+        child: Text('Start'),
+      ),
     );
   }
 }
--- lib/models/timer_model.dart
+++ lib/models/timer_model.dart
@@ -1,35 +1,35 @@
-
-import 'package:flutter/material.dart';
-
-class Item extends StatelessWidget {
-  String title = 'Timed Stage';
-  String description = 'An item that is timed.';
-  int total = 60;
-
-  @override
-  Widget build(BuildContext context) {
-    return Text('Hello');
-  }
-}
-
-class TimerModel extends ChangeNotifier {
-  /// Internal, private state of the cart.
-  final List<Item> _items = [];
-
-  /// An unmodifiable view of the items in the cart.
-  ListView get items => ListView(children: _items);
-
-  /// The current total time of all items
-  int get totalTime => _items.length * 12;
-
-  void add(Item item) {
-    _items.add(item);
-    // This call tells the widgets that are listening to this model to rebuild.
-    notifyListeners();
-  }
-
-  void removeAll() {
-    _items.clear();
-    notifyListeners();
-  }
-}
+//
+//import 'package:flutter/material.dart';
+//
+//class Item extends StatelessWidget {
+//  String title = 'Timed Stage';
+//  String description = 'An item that is timed.';
+//  int total = 60;
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return Text('Hello');
+//  }
+//}
+//
+//class TimerModel extends ChangeNotifier {
+//  /// Internal, private state of the cart.
+//  final List<Item> _items = [];
+//
+//  /// An unmodifiable view of the items in the cart.
+//  ListView get items => ListView(children: _items);
+//
+//  /// The current total time of all items
+//  int get totalTime => _items.length * 12;
+//
+//  void add(Item item) {
+//    _items.add(item);
+//    // This call tells the widgets that are listening to this model to rebuild.
+//    notifyListeners();
+//  }
+//
+//  void removeAll() {
+//    _items.clear();
+//    notifyListeners();
+//  }
+//}
